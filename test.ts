841af5a04ae701aba1741b7cfd2808f445f5af41
B06DAN3L2 <ionitabogdanmihai2013@gmail.com>
2024-12-24 20:44:04 +0200

    Update pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,4 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+AI.setvecsize(2)
+AI.addtodata([0, 1],"<")
+AI.addtodata([1, 0], ">")
+basic.showString(AI.clasify([5,2]))
