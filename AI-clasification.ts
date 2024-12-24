03b67beaca3707ec5a34dd355c268714348b5fd0
B06DAN3L2 <ionitabogdanmihai2013@gmail.com>
2024-12-24 20:41:53 +0200

    Initial files for MakeCode project

---
+++ AI-clasification.ts
@@ -0,0 +1,59 @@
+
+//AI clasification
+let trainingdataVec = [[0]]
+let trainingdataClass = [""]
+let startofTrainingData = true
+let vectorsize = 0
+
+//% color=340
+//% color="#d79df5"
+//% icon=""
+
+namespace AI {
+    function distance(vec1: number[], vec2: number[]) {
+        let distsquared = 0
+        for (let i = 0; i < vec1.length; i++) {
+            distsquared += (vec1[i] - vec2[i]) * (vec1[i] - vec2[i])
+        }
+        return distsquared
+    }
+    //% block="set vector dimensionality (size) to $size"
+    export function setvecsize(size: number) {
+        vectorsize = size
+    }
+    //% block="add to training dataset vector $vector , with classification $classname"
+    export function addtodata(vector: number[], classname: string) {
+        if (startofTrainingData) {
+            trainingdataVec = [vector]
+            trainingdataClass = [classname]
+            startofTrainingData = false
+        } else {
+            trainingdataVec.push(vector)
+            trainingdataClass.push(classname)
+        }
+    }
+    //% block="clasify vector $vector"
+    export function clasify(vector: number[]) {
+        let neardist = Infinity
+        let nearclass = ""
+        let temp = 0
+        for (let i = 0; i < trainingdataClass.length; i++) {
+            temp = distance(vector, trainingdataVec[i])
+            if (temp == 0) {
+                nearclass = trainingdataClass[i]
+                break
+            }
+            if (neardist > temp) {
+                neardist = temp
+                nearclass = trainingdataClass[i]
+            }
+        }
+        return nearclass
+    }
+    //% block="delete training dataset"
+    export function deletetrainingdata() {
+        trainingdataVec = [[0]]
+        trainingdataClass = [""]
+        startofTrainingData = true
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+**"AI Clasification"** is an extension for Micro:bit that, does exactly what you expect.
+
+Blocks:
+
+  •**"Set vector dimensionality to (size)"**
+
+  •**"Add to training dataset vector (vector) , with classification (classname)"**
+
+  •**"Clasify vector (vector)"**:It returns the classification of the training dataset's vector that is closest to the input vector using euclidean distance 
+
+  •**"Delete training dataset"**
+
+
+> Open this page at [https://b06danel.github.io/ai-clasification/](https://b06danel.github.io/ai-clasification/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/b06danel/ai-clasification** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/b06danel/ai-clasification** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
